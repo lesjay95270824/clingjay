ed74dba289219e3fbbed15ce8f0bc4d1f83f2925
lesjay95270824 <62696911+lesjay95270824@users.noreply.github.com>
2020-03-26 23:37:18 +0800

    优化递归的次数
    将递归中重复计算的数，通过数组的方式去减少递归的次数

--- mian.cpp
+++ mian.cpp
@@ -1,19 +1,25 @@
 #include<iostream>
-
+#include<vector>
 using namespace std;
 
+vector<int> memo;
 long fib(int n)
 {
 if(n==0)
   return 0;
 if(n==1)
 return 1;
-return fib(n-1)+fib(n-2);
+  if(memo[n]==-1)
+  {
+   memo[n]=fib(n-1)+fib(n-2);
+  }
+return memo[n];
 }
 
 int main()
 {
  int n=50;
+  memo=vector<int>(n+1,-1)
  long res=fib(n);
  cout<<"fib（"<<n<<"）="<<res<<endl;
  return 0;
